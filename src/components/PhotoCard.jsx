3112c2d51f85b65bebc8e8a1073cf56c89d57075
A.Yaswanth Kiran <yaswanthjk580@gmail.com>
2024-10-06 13:17:41 +0530

    chnages

--- src/components/PhotoCard.jsx
+++ src/components/PhotoCard.jsx
@@ -4,4 +4,4 @@ function PhotoCard(){
   return <img className="PhotoCard" src="https://raw.githubusercontent.com/yash5800/ND_store/master/20240812-101519_Screenshot(10).png" alt="img" />
 }
 
-export {PhotoCard}
+export { PhotoCard }
